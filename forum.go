daee9223b819ecea65ed227437076dcc12117847
John Sandfort <garnavis@gmail.com>
2017-11-25 14:25:03 -0500

    Add error handling for handlers

--- forum.go
+++ forum.go
@@ -12,12 +12,20 @@ type Post struct {
 var post = &Post{Body: "Nothing to see here"}
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("index.html")
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	t.Execute(w, post)
 }
 
 func newHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("new.html")
+	t, err := template.ParseFiles("new.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	t.Execute(w, &Post{})
 }
 
