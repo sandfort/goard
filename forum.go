7f055c0adfc568598abd822b66c69dcc0f82ae79
John Sandfort <garnavis@gmail.com>
2017-11-25 14:05:11 -0500

    Extract body to variable

--- forum.go
+++ forum.go
@@ -5,8 +5,10 @@ import (
 	"net/http"
 )
 
+var body = "Nothing to see here"
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<p>Nothing to see here</p>")
+	fmt.Fprintf(w, "<p>%s</p>", body)
 }
 
 func main() {
