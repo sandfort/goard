bb68cce3f934e8706aaf3a28ac3db5953d82974b
John Sandfort <garnavis@gmail.com>
2017-11-28 13:24:16 -0500

    Add post view
    Also:
    - add "posts" rooted subtree to paths
    - change index to link to post views

--- forum.go
+++ forum.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 type Post struct {
@@ -34,12 +35,27 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.FormValue("title")
 	body := r.FormValue("body")
 	posts = append(posts, Post{Title: title, Body: body})
-	http.Redirect(w, r, "/", http.StatusFound)
+	http.Redirect(w, r, "/posts", http.StatusFound)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Path[len("/posts/"):])
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	t, err := template.ParseFiles("view.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w, posts[id])
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/new", newHandler)
-	http.HandleFunc("/save", saveHandler)
+	http.HandleFunc("/posts", handler)
+	http.HandleFunc("/posts/new", newHandler)
+	http.HandleFunc("/posts/save", saveHandler)
+	http.HandleFunc("/posts/", viewHandler)
 	http.ListenAndServe(":8080", nil)
 }
